1c884e78f596972ceb656b76fdf39846f8b7ba4e
Sander Boer <sanderboer@mauc.nl>
2022-09-05 22:43:40 +0200

    Too lazy to comment

--- src/enums.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum CameraType {
-    perspective = "perspective",
-    orthogonal = "orthogonal"
-}
-export declare enum ViewportSide {
-    left = "left",
-    right = "right",
-    total = "total"
-}
--- src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { WebViewer } from "./webviewer";
